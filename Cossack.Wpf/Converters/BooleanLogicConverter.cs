5a3a160610c41af7a5170b3701b60291b73a6d70
David Norris <danorris@gmail.com>
2018-06-25 09:51:00 -0500

    Added boolean logic multi-value converter (N/AND, N/OR, XOR/XNOR).

---
+++ Cossack.Wpf/Converters/BooleanLogicConverter.cs
@@ -0,0 +1,81 @@
+﻿//
+// Copyright (C) 2018 David A. Norris <danorris@gmail.com>. All rights reserved.
+//
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Cossack.Wpf.Converters
+{
+	/// <summary>
+	/// A multi-value converter which returns the result of boolean logic on one or more boolean
+	/// inputs. The converter can operate in boolean-AND or boolean-OR mode.
+	/// </summary>
+
+	public class BooleanLogicConverter : IMultiValueConverter
+	{
+		/// <summary>
+		/// Converts the input path by applying the specified boolean logic to the inputs.
+		/// </summary>
+		///
+		/// <param name="values">The input values to convert.</param>
+		/// <param name="targetType">Ignored.</param>
+		/// <param name="parameter">Ignored.</param>
+		/// <param name="culture">Ignored.</param>
+
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			switch (Mode)
+			{
+				case BooleanLogicConverterMode.And:
+					return values.All(v => true.Equals(v));
+
+				case BooleanLogicConverterMode.Or:
+					return values.Any(v => true.Equals(v));
+
+				case BooleanLogicConverterMode.Nand:
+					return !values.All(v => true.Equals(v));
+
+				case BooleanLogicConverterMode.Nor:
+					return !values.Any(v => true.Equals(v));
+
+				case BooleanLogicConverterMode.Xor:
+					return values.Count(v => true.Equals(v)) % 2 == 1;
+
+				case BooleanLogicConverterMode.Xnor:
+					return values.Count(v => true.Equals(v)) % 2 == 0;
+
+				default:
+					throw new NotSupportedException();
+			}
+		}
+
+		/// <summary>
+		/// Throws <see cref="NotSupportedException"/> because backward conversion is not
+		/// supported.
+		/// </summary>
+		///
+		/// <param name="value">Ignored.</param>
+		/// <param name="targetTypes">Ignored.</param>
+		/// <param name="parameter">Ignored.</param>
+		/// <param name="culture">Ignored.</param>
+		///
+		/// <returns>This method does not return.</returns>
+		///
+		/// <exception cref="NotSupportedException">This method is not supported.</exception>
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Gets or sets a flag specifying whether this converter should operate in AND, OR, NAND,
+		/// NOR, XOR or XNOR mode.
+		/// </summary>
+
+		public BooleanLogicConverterMode Mode { get; set; } = BooleanLogicConverterMode.And;
+	}
+}
---
+++ Cossack.Wpf/Converters/BooleanLogicConverterMode.cs
@@ -0,0 +1,56 @@
+﻿//
+// Copyright (C) 2018 David A. Norris <danorris@gmail.com>. All rights reserved.
+//
+
+namespace Cossack.Wpf.Converters
+{
+	/// <summary>
+	/// Specifies whether a <see cref="BooleanLogicConverter"/> should operate in AND, OR, NAND,
+	/// NOR, XOR or XNOR mode.
+	/// </summary>
+
+	public enum BooleanLogicConverterMode
+	{
+		/// <summary>
+		/// Indicates that the <see cref="BooleanLogicConverter"/> should operate in AND mode,
+		/// where the output is true if and only if all inputs are true.
+		/// </summary>
+
+		And,
+
+		/// <summary>
+		/// Indicates that the <see cref="BooleanLogicConverter"/> should operate in OR mode,
+		/// where the output is true if and only if any input is true.
+		/// </summary>
+
+		Or,
+
+		/// <summary>
+		/// Indicates that the <see cref="BooleanLogicConverter"/> should operate in NAND mode,
+		/// where the output is true if and only if any input is false.
+		/// </summary>
+
+		Nand,
+
+		/// <summary>
+		/// Indicates that the <see cref="BooleanLogicConverter"/> should operate in NOR mode,
+		/// where the output is true if and only if all inputs are false.
+		/// </summary>
+
+		Nor,
+
+		/// <summary>
+		/// Indicates that the <see cref="BooleanLogicConverter"/> should operate in XOR mode,
+		/// where the output is true if and only if an odd number of inputs are true.
+		/// </summary>
+
+		Xor,
+
+		/// <summary>
+		/// Indicates that the <see cref="BooleanLogicConverter"/> should operate in XNOR mode,
+		/// where the output is true if and only if an even number of inputs are true.
+		/// </summary>
+
+		Xnor,
+	}
+}
