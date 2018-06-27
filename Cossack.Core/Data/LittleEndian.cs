1cfb702e6800bb939d2fd3e866b3f6f6d32d548e
David Norris <danorris@gmail.com>
2018-06-26 23:39:43 -0500

    Added more Read/Write methods, ditched CLS compliance, minor tweaks.

--- Cossack.Core/Data/LittleEndian.cs
+++ Cossack.Core/Data/LittleEndian.cs
@@ -8,13 +8,13 @@ namespace Cossack.Core.Data
 {
 	/// <summary>
 	/// Contains utility methods for encoding and decoding primitive numeric values as
-	/// big-endian byte sequences.
+	/// little-endian byte sequences.
 	/// </summary>
 
-	public static class BigEndian
+	public static class LittleEndian
 	{
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 16-bit unsigned integer.
+		/// Decodes a little-endian byte sequence as a 16-bit unsigned integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -32,13 +32,13 @@ namespace Cossack.Core.Data
 		public static ushort ReadU16(byte[] buffer, int offset)
 		{
 			return (ushort) (
-				(buffer[offset + 0] << 8) |
-				(buffer[offset + 1] << 0)
+				(buffer[offset + 0] << 0) |
+				(buffer[offset + 1] << 8)
 			);
 		}
 
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 16-bit signed integer.
+		/// Decodes a little-endian byte sequence as a 16-bit signed integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -55,13 +55,13 @@ namespace Cossack.Core.Data
 		public static short ReadS16(byte[] buffer, int offset)
 		{
 			return (short) (
-				(buffer[offset + 0] << 8) |
-				(buffer[offset + 1] << 0)
+				(buffer[offset + 0] << 0) |
+				(buffer[offset + 1] << 8)
 			);
 		}
 
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 24-bit unsigned integer.
+		/// Decodes a little-endian byte sequence as a 24-bit unsigned integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -79,13 +79,13 @@ namespace Cossack.Core.Data
 		public static uint ReadU24(byte[] buffer, int offset)
 		{
 			return
-				((uint) buffer[offset + 0] << 16) |
+				((uint) buffer[offset + 0] << 0) |
 				((uint) buffer[offset + 1] << 8) |
-				((uint) buffer[offset + 2] << 0);
+				((uint) buffer[offset + 2] << 16);
 		}
 
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 24-bit signed integer.
+		/// Decodes a little-endian byte sequence as a 24-bit signed integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -102,13 +102,13 @@ namespace Cossack.Core.Data
 		public static int ReadS24(byte[] buffer, int offset)
 		{
 			return
-				((buffer[offset + 0] << 24) >> 8) |
-				((int) buffer[offset + 1] << 8) |
-				((int) buffer[offset + 2] << 0);
+				(buffer[offset + 0] << 0) |
+				(buffer[offset + 1] << 8) |
+				((buffer[offset + 2] << 24) >> 8);
 		}
 
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 32-bit unsigned integer.
+		/// Decodes a little-endian byte sequence as a 32-bit unsigned integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -126,14 +126,14 @@ namespace Cossack.Core.Data
 		public static uint ReadU32(byte[] buffer, int offset)
 		{
 			return
-				((uint) buffer[offset + 0] << 24) |
-				((uint) buffer[offset + 1] << 16) |
-				((uint) buffer[offset + 2] << 8) |
-				((uint) buffer[offset + 3] << 0);
+				((uint) buffer[offset + 0] << 0) |
+				((uint) buffer[offset + 1] << 8) |
+				((uint) buffer[offset + 2] << 16) |
+				((uint) buffer[offset + 3] << 24);
 		}
 
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 32-bit signed integer.
+		/// Decodes a little-endian byte sequence as a 32-bit signed integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -150,14 +150,14 @@ namespace Cossack.Core.Data
 		public static int ReadS32(byte[] buffer, int offset)
 		{
 			return
-				(buffer[offset + 0] << 24) |
-				(buffer[offset + 1] << 16) |
-				(buffer[offset + 2] << 8) |
-				(buffer[offset + 3] << 0);
+				(buffer[offset + 0] << 0) |
+				(buffer[offset + 1] << 8) |
+				(buffer[offset + 2] << 16) |
+				(buffer[offset + 3] << 24);
 		}
 
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 64-bit unsigned integer.
+		/// Decodes a little-endian byte sequence as a 64-bit unsigned integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -175,18 +175,18 @@ namespace Cossack.Core.Data
 		public static ulong ReadU64(byte[] buffer, int offset)
 		{
 			return
-				((ulong) buffer[offset + 0] << 56) |
-				((ulong) buffer[offset + 1] << 48) |
-				((ulong) buffer[offset + 2] << 40) |
-				((ulong) buffer[offset + 3] << 32) |
-				((ulong) buffer[offset + 4] << 24) |
-				((ulong) buffer[offset + 5] << 16) |
-				((ulong) buffer[offset + 6] << 8) |
-				((ulong) buffer[offset + 7] << 0);
+				((ulong) buffer[offset + 0] << 0) |
+				((ulong) buffer[offset + 1] << 8) |
+				((ulong) buffer[offset + 2] << 16) |
+				((ulong) buffer[offset + 3] << 24) |
+				((ulong) buffer[offset + 4] << 32) |
+				((ulong) buffer[offset + 5] << 40) |
+				((ulong) buffer[offset + 6] << 48) |
+				((ulong) buffer[offset + 7] << 56);
 		}
 
 		/// <summary>
-		/// Decodes a big-endian byte sequence as a 64-bit signed integer.
+		/// Decodes a little-endian byte sequence as a 64-bit signed integer.
 		/// </summary>
 		///
 		/// <param name="buffer">The array containing the bytes to decode.</param>
@@ -203,18 +203,18 @@ namespace Cossack.Core.Data
 		public static long ReadS64(byte[] buffer, int offset)
 		{
 			return
-				((long) buffer[offset + 0] << 56) |
-				((long) buffer[offset + 1] << 48) |
-				((long) buffer[offset + 2] << 40) |
-				((long) buffer[offset + 3] << 32) |
-				((long) buffer[offset + 4] << 24) |
-				((long) buffer[offset + 5] << 16) |
-				((long) buffer[offset + 6] << 8) |
-				((long) buffer[offset + 7] << 0);
+				((long) buffer[offset + 0] << 0) |
+				((long) buffer[offset + 1] << 8) |
+				((long) buffer[offset + 2] << 16) |
+				((long) buffer[offset + 3] << 24) |
+				((long) buffer[offset + 4] << 32) |
+				((long) buffer[offset + 5] << 40) |
+				((long) buffer[offset + 6] << 48) |
+				((long) buffer[offset + 7] << 56);
 		}
 
 		/// <summary>
-		/// Encodes a 16-bit unsigned integer as a big-endian byte sequence.
+		/// Encodes a 16-bit unsigned integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -230,12 +230,12 @@ namespace Cossack.Core.Data
 		[CLSCompliant(false)]
 		public static void WriteU16(byte[] buffer, int offset, ushort value)
 		{
-			buffer[offset + 0] = (byte) (value >> 8);
-			buffer[offset + 1] = (byte) (value >> 0);
+			buffer[offset + 0] = (byte) (value >> 0);
+			buffer[offset + 1] = (byte) (value >> 8);
 		}
 
 		/// <summary>
-		/// Encodes a 16-bit signed integer as a big-endian byte sequence.
+		/// Encodes a 16-bit signed integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -250,12 +250,12 @@ namespace Cossack.Core.Data
 
 		public static void WriteS16(byte[] buffer, int offset, short value)
 		{
-			buffer[offset + 0] = (byte) (value >> 8);
-			buffer[offset + 1] = (byte) (value >> 0);
+			buffer[offset + 0] = (byte) (value >> 0);
+			buffer[offset + 1] = (byte) (value >> 8);
 		}
 
 		/// <summary>
-		/// Encodes a 24-bit unsigned integer as a big-endian byte sequence.
+		/// Encodes a 24-bit unsigned integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -272,13 +272,13 @@ namespace Cossack.Core.Data
 		[CLSCompliant(false)]
 		public static void WriteU24(byte[] buffer, int offset, uint value)
 		{
-			buffer[offset + 0] = (byte) (value >> 16);
+			buffer[offset + 0] = (byte) (value >> 0);
 			buffer[offset + 1] = (byte) (value >> 8);
-			buffer[offset + 2] = (byte) (value >> 0);
+			buffer[offset + 2] = (byte) (value >> 16);
 		}
 
 		/// <summary>
-		/// Encodes a 24-bit signed integer as a big-endian byte sequence.
+		/// Encodes a 24-bit signed integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -294,13 +294,13 @@ namespace Cossack.Core.Data
 
 		public static void WriteS24(byte[] buffer, int offset, int value)
 		{
-			buffer[offset + 0] = (byte) (value >> 16);
+			buffer[offset + 0] = (byte) (value >> 0);
 			buffer[offset + 1] = (byte) (value >> 8);
-			buffer[offset + 2] = (byte) (value >> 0);
+			buffer[offset + 2] = (byte) (value >> 16);
 		}
 
 		/// <summary>
-		/// Encodes a 32-bit unsigned integer as a big-endian byte sequence.
+		/// Encodes a 32-bit unsigned integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -316,14 +316,14 @@ namespace Cossack.Core.Data
 		[CLSCompliant(false)]
 		public static void WriteU32(byte[] buffer, int offset, uint value)
 		{
-			buffer[offset + 0] = (byte) (value >> 24);
-			buffer[offset + 1] = (byte) (value >> 16);
-			buffer[offset + 2] = (byte) (value >> 8);
-			buffer[offset + 3] = (byte) (value >> 0);
+			buffer[offset + 0] = (byte) (value >> 0);
+			buffer[offset + 1] = (byte) (value >> 8);
+			buffer[offset + 2] = (byte) (value >> 16);
+			buffer[offset + 3] = (byte) (value >> 24);
 		}
 
 		/// <summary>
-		/// Encodes a 32-bit signed integer as a big-endian byte sequence.
+		/// Encodes a 32-bit signed integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -338,15 +338,15 @@ namespace Cossack.Core.Data
 
 		public static void WriteS32(byte[] buffer, int offset, int value)
 		{
-			buffer[offset + 0] = (byte) (value >> 24);
-			buffer[offset + 1] = (byte) (value >> 16);
-			buffer[offset + 2] = (byte) (value >> 8);
-			buffer[offset + 3] = (byte) (value >> 0);
+			buffer[offset + 0] = (byte) (value >> 0);
+			buffer[offset + 1] = (byte) (value >> 8);
+			buffer[offset + 2] = (byte) (value >> 16);
+			buffer[offset + 3] = (byte) (value >> 24);
 		}
 
 
 		/// <summary>
-		/// Encodes a 64-bit unsigned integer as a big-endian byte sequence.
+		/// Encodes a 64-bit unsigned integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -362,18 +362,18 @@ namespace Cossack.Core.Data
 		[CLSCompliant(false)]
 		public static void WriteU64(byte[] buffer, int offset, ulong value)
 		{
-			buffer[offset + 0] = (byte) (value >> 56);
-			buffer[offset + 1] = (byte) (value >> 48);
-			buffer[offset + 2] = (byte) (value >> 40);
-			buffer[offset + 3] = (byte) (value >> 32);
-			buffer[offset + 4] = (byte) (value >> 24);
-			buffer[offset + 5] = (byte) (value >> 16);
-			buffer[offset + 6] = (byte) (value >> 8);
-			buffer[offset + 7] = (byte) (value >> 0);
+			buffer[offset + 0] = (byte) (value >> 0);
+			buffer[offset + 1] = (byte) (value >> 8);
+			buffer[offset + 2] = (byte) (value >> 16);
+			buffer[offset + 3] = (byte) (value >> 24);
+			buffer[offset + 4] = (byte) (value >> 32);
+			buffer[offset + 5] = (byte) (value >> 40);
+			buffer[offset + 6] = (byte) (value >> 48);
+			buffer[offset + 7] = (byte) (value >> 56);
 		}
 
 		/// <summary>
-		/// Encodes a 64-bit signed integer as a big-endian byte sequence.
+		/// Encodes a 64-bit signed integer as a little-endian byte sequence.
 		/// </summary>
 		///
 		/// <param name="buffer">The array in which to store the encoded bytes.</param>
@@ -388,14 +388,14 @@ namespace Cossack.Core.Data
 
 		public static void WriteS64(byte[] buffer, int offset, long value)
 		{
-			buffer[offset + 0] = (byte) (value >> 56);
-			buffer[offset + 1] = (byte) (value >> 48);
-			buffer[offset + 2] = (byte) (value >> 40);
-			buffer[offset + 3] = (byte) (value >> 32);
-			buffer[offset + 4] = (byte) (value >> 24);
-			buffer[offset + 5] = (byte) (value >> 16);
-			buffer[offset + 6] = (byte) (value >> 8);
-			buffer[offset + 7] = (byte) (value >> 0);
+			buffer[offset + 0] = (byte) (value >> 0);
+			buffer[offset + 1] = (byte) (value >> 8);
+			buffer[offset + 2] = (byte) (value >> 16);
+			buffer[offset + 3] = (byte) (value >> 24);
+			buffer[offset + 4] = (byte) (value >> 32);
+			buffer[offset + 5] = (byte) (value >> 40);
+			buffer[offset + 6] = (byte) (value >> 48);
+			buffer[offset + 7] = (byte) (value >> 56);
 		}
 	}
 }
